992f34d1fccd7ae9b1a71c2118d439bce36d53ae
Andy Armstrong <andy@hexten.net>
2007-10-03 17:53:15 +0000

    Version bump

--- lib/Parallel/Iterator.pm
+++ lib/Parallel/Iterator.pm
@@ -11,7 +11,7 @@ use Config;
 
 require 5.008;
 
-our $VERSION = '0.3.0';
+our $VERSION = '0.4.0';
 use base qw( Exporter );
 our @EXPORT_OK = qw( iterate iterate_as_array iterate_as_hash );
 
@@ -26,7 +26,7 @@ Parallel::Iterator - Simple parallel execution
 
 =head1 VERSION
 
-This document describes Parallel::Iterator version 0.3.0
+This document describes Parallel::Iterator version 0.4.0
 
 =head1 SYNOPSIS
 
