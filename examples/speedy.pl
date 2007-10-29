1fe9232a52fd7a5ae8f7d9301502a8144c75c0a5
Andy Armstrong <andy@hexten.net>
2007-10-29 19:46:17 +0000

    Added documentation to example

--- examples/speedy.pl
+++ examples/speedy.pl
@@ -5,6 +5,11 @@ use warnings;
 use Inline 'C';
 use Parallel::Iterator qw( iterate_as_array );
 
+# Demonstrates a simple way to run multiple instances of a C function in
+# parallel. You'll need it to be a fairly time consuming function
+# otherwise the overhead of forking and marshalling arguments will
+# overwhelm the execution time.
+
 my @ar = qw( This Pork Bubble );
 @ar = ( @ar, @ar ) for 1 .. 5;
 my @got = iterate_as_array( sub { calc( $_[1] ) }, \@ar );
